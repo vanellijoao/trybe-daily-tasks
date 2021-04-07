f173b7a56bf3f35832f76c3b861e9651d65cd56e
Joao Vanelli <vanellijoao@gmail.com>
2021-04-06 22:21:25 -0300

    Bloco 8.4 - Ex 1 e 2

---
+++ tasks/8/8.4/ex1.js
@@ -0,0 +1,16 @@
+
+const assert = require('assert');
+
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+];
+
+function flatten() { 
+  return arrays.reduce((acc, arr) => acc.concat(arr) , [])
+}
+
+console.log(flatten());
+
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
