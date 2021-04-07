932ba424bdc4e0622e5b8c4b6b9b8a6707d63b98
Joao Vanelli <vanellijoao@gmail.com>
2021-04-07 16:05:40 -0300

    Bloco 8.5 - Ex 3 e 4

---
+++ tasks/8/8.5/ex4.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+const filterPeople = () => {
+  return people.reduce((sec20, person) => {
+    const {bornIn} = person;
+    return bornIn < 2001 ? [...sec20, person] : [...sec20]
+  }, []) 
+};
+
+const filteredPeople = filterPeople(people);
+
+console.log(filteredPeople)
+
+assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
+assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });
